f433800f5684d9a47bfcd04c090cbf71e987aeec
anjali <40533147+anjali2055a@users.noreply.github.com>
2022-02-02 15:52:17 +0545

    Updated Electricity Bill Program

--- Program.cs
+++ Program.cs
@@ -183,49 +183,29 @@ switch (choice)
 }*/
 
 
+/*Write the program for Nepal Telecommunication Corporation that charges the following form its domestic users:
+First 100 calls(Compulsory fee)    Rs .150
+Next call 100 calls                 Rs.2.00 per Call
+Above this                         Rs.2.50 per call*/
 
-int custid, conu;
-   double chg, surchg=0, gramt,netamt;
-   string connm;
+Console.WriteLine("Enter the number of Calls:");
+double Tcall = Convert.ToDouble(Console.ReadLine());
+double Total;
+if(Tcall<100)
+{
+	Console.WriteLine("Total is Rs.150");
 
-           Console.Write("\n\n");
-           Console.Write("Calculate Electricity Bill:\n");
-           Console.Write("----------------------------");
-           Console.Write("\n\n");
-
-   Console.Write("Input Customer ID :");
-   custid= Convert.ToInt32(Console.ReadLine());
-   Console.Write("Input the name of the customer :");
-   connm= Console.ReadLine();
-   Console.Write("Input the unit consumed by the customer : ");
-   conu= Convert.ToInt32(Console.ReadLine());
-   if (conu <200 )
-	chg = 1.20;
-   else	if (conu>=200 && conu<400)
-		chg = 1.50;
-	else if (conu>=400 && conu<600)
-			chg = 1.80;
-		else
-			chg = 2.00;
-   gramt = conu*chg;
-   if (gramt>300)
-	surchg = gramt*15/100.0;
-   netamt = gramt+surchg;
-   if (netamt  < 100)
-	netamt =100;
-   Console.Write("\nElectricity Bill\n");
-   Console.Write("Customer IDNO                       :{0}\n",custid);
-   Console.Write("Customer Name                       :{0}\n",connm);
-   Console.Write("unit Consumed                       :{0}\n",conu);
-   Console.Write("Amount Charges @Rs. {0}  per unit :{1}\n",chg,gramt);
-   Console.Write("Surchage Amount                     :{0}\n",surchg);
-   Console.Write("Net Amount Paid By the Customer     :{0}\n",netamt);
-}  
-} 
-
-
-
-
+}
+else if(Tcall<200)
+{
+	Total = 150 + (2 * (Tcall - 100));
+	Console.WriteLine("Total :" + Total);
+}
+else
+{
+	Total = 350 + (2.5 * (Tcall - 200));// Substracting cost of 200 calls 
+	Console.WriteLine("Total :" + Total);
+}
 
 
 
